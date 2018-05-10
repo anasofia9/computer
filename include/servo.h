213980f701b0cc9bbd7526d8a7336253056b428b
anasofia9 <30392129+anasofia9@users.noreply.github.com>
2018-05-10 10:46:09 -0700

    Delete servo.h

--- include/servo.h
+++
@@ -1,9 +0,0 @@
-#ifndef SERVO_H
-#define SERVO_H
-
-
-void servo_init(void);
-void servo_set_pwm(unsigned int servoNum, unsigned int percentOn);
-void servo_set_position(unsigned int servoNum, unsigned int pwm);
-
-#endif
